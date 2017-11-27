38f2ef0f0ea2bee950affa6ece60c32bd3aa18e0
solufemi91 <stevenoo@spartaglobal.com>
2017-11-27 16:44:28 +0000

    the save function for when a new booking is made is working

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -26,7 +26,7 @@ class BookingsController < ApplicationController
   # POST /bookings.json
   def create
     @booking = Booking.new(booking_params)
-
+    # @rooms = Room.all
 
     respond_to do |format|
       if @booking.save
@@ -71,6 +71,6 @@ class BookingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def booking_params
-      params.require(:booking).permit(:scaffold_controller, :description, :date_in, :date_out)
+      params.require(:booking).permit(:scaffold_controller, :description, :date_in, :date_out, :room_id)
     end
 end
