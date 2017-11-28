e8fc0fbc0cb01eadedb7fa987bafcbd783406588
Tech-A29 <Tech-A29@Tech-A29s-MacBook-Air.local>
2017-11-28 16:19:46 +0000

    delete works

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -16,7 +16,7 @@ class RoomsController < ApplicationController
   # GET /rooms/1.json
   def show
     @rooms = Room.all
-
+    @bookings = Booking.all
   end
 
   # GET /rooms/new
@@ -61,7 +61,6 @@ class RoomsController < ApplicationController
   # DELETE /rooms/1
   # DELETE /rooms/1.json
   def destroy
-
     @room.destroy
     respond_to do |format|
       format.html { redirect_to rooms_url, notice: 'Room was successfully destroyed.' }
