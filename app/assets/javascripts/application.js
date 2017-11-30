6e667a33fddffcc0cc108c6aa4d7cabfe27a6a80
Tech-A29 <Tech-A29@Tech-A29s-MacBook-Air.local>
2017-11-30 14:47:30 +0000

    arrow pop up form works

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,18 @@
 //= require bootstrap-sprockets
 //= require moment
 //= require bootstrap-datetimepicker
+
+jQuery(function($) {
+		var open = false;
+		$('#footerSlideButton').click(function () {
+			if(open === false) {
+				$('#footerSlideContent').animate({ height: '195px' });
+				$(this).css('backgroundPosition', 'bottom left');
+				open = true;
+			} else {
+				$('#footerSlideContent').animate({ height: '0px' });
+				$(this).css('backgroundPosition', 'top left');
+				open = false;
+			}
+		});
+	});
